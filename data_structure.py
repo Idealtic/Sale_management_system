04cbf8c2e7b07bb32e7a8e67a48979a8f7add9c9
Idealtic <hoangphamminh85@gmail.com>
2026-06-08 16:25:38 +0700

    update

--- data_structure.py
+++ data_structure.py
@@ -1,6 +1,13 @@
-class DoubleLinkedList:
+class DoublyNode:
     def __init__(self, val, next = None, prev = None):
         self.val = val 
         self.next = next
         self.prev = prev
 
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def append(self, val):
+        pass
