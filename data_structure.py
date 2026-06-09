649defb5336a4e4d22df99982d9368e1848bdb9d
Idealtic <hoangphamminh85@gmail.com>
2026-06-09 22:16:50 +0700

    updated doubly linked list

--- data_structure.py
+++ data_structure.py
@@ -1,4 +1,4 @@
-class DoublyNode:
+class DoublyNode: # Nút trong danh sách liên kết đôi
     def __init__(self, val, next = None, prev = None):
         self.val = val 
         self.next = next
@@ -10,7 +10,7 @@ class DoublyLinkedList: # Danh sách liên kết đôi
         self.tail = None
         self.size = 0
 
-    def append(self, val):
+    def append(self, val): # Thêm vào cuối danh sách
         new_node = DoublyNode(val)
 
         if self.head is None: 
@@ -22,7 +22,7 @@ class DoublyLinkedList: # Danh sách liên kết đôi
 
         self.size += 1
 
-    def prepend(self, val):
+    def prepend(self, val): # Thêm vào đầu danh sách
         new_node = DoublyNode(val)
 
         if self.tail is None:
@@ -34,16 +34,14 @@ class DoublyLinkedList: # Danh sách liên kết đôi
 
         self.size += 1
 
-    def insert_at(self, index, val):
+    def insert_at(self, index, val): # Thêm vào index bất kỳ (bắt đầu từ 0)
         if index < 0 or index > self.size:
             print("Lỗi: Index vượt quá giới hạn của danh sách!")
             return
         if index == 0:
-            self.prepend(val)
-            return
+            return self.prepend(val)
         if index == self.size:
-            self.append(val)
-            return
+            return self.append(val)
 
         new_node = DoublyNode(val)
         if index < self.size // 2:
@@ -62,12 +60,58 @@ class DoublyLinkedList: # Danh sách liên kết đôi
         curr.prev = new_node
 
         self.size += 1
+
+    def remove_head(self): # Xoá phần tử đầu tiên của danh sách
+        if self.head is None:
+            return
+        
+        if self.head == self.tail:
+            self.head = self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+
+        self.size -= 1
+    
+    def remove_tail(self): # Xoá phần tử cuối cùng của danh sách
+        if self.head is None:
+            return
+        
+        if self.head == self.tail:
+            self.head = self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+
+        self.size -= 1
+
+    def remove_at(self, index): # Xoá phần tử bất kỳ của danh sách
+        if index < 0 or index > self.size:
+            print("Lỗi: Index vượt quá giới hạn của danh sách!")
+            return
+        if index == 0:
+            return self.remove_head()
+        if index == self.size - 1:
+            return self.remove_tail()
+
+        if index < self.size // 2:
+            curr = self.head
+            for _ in range(index):
+                curr = curr.next
+        else:
+            curr = self.tail
+            for _ in range(self.size - 1 - index):
+                curr = curr.prev
+        curr.prev.next = curr.next
+        curr.next.prev = curr.prev
+
+        self.size -= 1
     
     def __len__(self):
         return self.size 
 
     def __iter__(self):
-        current = self.head         
-        while current is not None:  
-            yield current.val       
-            current = current.next  
+        curr = self.head         
+        while curr is not None:  
+            yield curr.val       
+            curr = curr.next  
